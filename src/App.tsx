784cb6d1988772808e038b8feb2ae38b9974a7dc
Tomoya AMACHI <tomoya.amachi@gmail.com>
2025-04-11 05:53:17 +0000

    refresh atomの動作テスト作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
 import "./App.css";
-import { DelayedComponent } from "./components/Delay.tsx";
-import { Suspense } from "react";
-import { Counter } from "./components/Counter.tsx";
-import { StoreCounter } from "./components/StoreCounter.tsx";
-import { DevTools } from 'jotai-devtools'
+import {DelayedComponent} from "./components/Delay.tsx";
+import {Suspense} from "react";
+import {Counter} from "./components/Counter.tsx";
+import {StoreCounter} from "./components/StoreCounter.tsx";
+import {DevTools} from 'jotai-devtools'
 import 'jotai-devtools/styles.css'
+import {postsAtom} from "./atoms.ts";
+import {useSetAtom} from "jotai/react";
 
 function App() {
 
-  return (
-    <>
-      <DevTools />
-      <div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <DelayedComponent/>
-        </Suspense>
-      </div>
-      <div className="App">
-        <Counter/>
-      </div>
-      <div className="App">
-        <StoreCounter/>
-      </div>
-    </>
-  );
+    const refreshAtom = useSetAtom(postsAtom)
+    return (
+        <>
+            <DevTools/>
+            <button onClick={() => {
+                refreshAtom();
+            }}>Force refresh
+            </button>
+            <div>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <DelayedComponent/>
+                </Suspense>
+            </div>
+            <div className="App">
+                <Counter/>
+            </div>
+            <div className="App">
+                <StoreCounter/>
+            </div>
+        </>
+    );
 }
-
-
-
 
 
 export default App;
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,12 @@
-import { atom } from "jotai/index";
+import {atom} from "jotai/index";
+import {atomWithRefresh} from "jotai/utils";
 
 export const countAtom = atom(0);
 export const storeCountAtom = atom(0);
+export const postsAtom = atomWithRefresh((get) => {
+        // trigger
+        get(countAtom);
+        console.log("calicurated");
+        return fetch('https://jsonplaceholder.typicode.com/posts').then((r) => r.json())
+    }
+)
